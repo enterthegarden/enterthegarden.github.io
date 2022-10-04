9119500eb0b2f2ec8b44940db309cf24131a5d06
enterthegarden <101602580+enterthegarden@users.noreply.github.com>
2022-10-04 07:47:08 +0100

    Add files via upload

---
+++ Seneker.js
@@ -0,0 +1,93 @@
+function funE1() {
+  funInvisible();
+  document.getElementById("E1").style.display = "block";
+}
+
+function funE2() {
+  funInvisible();
+  document.getElementById("E2").style.display = "block";
+}
+
+function funE3() {
+  funInvisible();
+  document.getElementById("E3").style.display = "block";
+}
+
+function funE4() {
+  funInvisible();
+  document.getElementById("E4").style.display = "block";
+}
+
+function funE5() {
+  funInvisible();
+  document.getElementById("E5").style.display = "block";
+}
+
+function funE6() {
+  funInvisible();
+  document.getElementById("E6").style.display = "block";
+}
+
+function funE7() {
+  funInvisible();
+  document.getElementById("E7").style.display = "block";
+}
+
+
+
+
+
+function funC1() {
+  funInvisible();
+  document.getElementById("C1").style.display = "block";
+}
+
+function funC2() {
+  funInvisible();
+  document.getElementById("C2").style.display = "block";
+}
+
+function funC3() {
+  funInvisible();
+  document.getElementById("C3").style.display = "block";
+}
+
+function funC4() {
+  funInvisible();
+  document.getElementById("C4").style.display = "block";
+}
+
+function funC5() {
+  funInvisible();
+  document.getElementById("C5").style.display = "block";
+}
+
+function funC6() {
+  funInvisible();
+  document.getElementById("C6").style.display = "block";
+}
+
+function funC7() {
+  funInvisible();
+  document.getElementById("C7").style.display = "block";
+}
+
+
+function funInvisible() {
+  document.getElementById("E1").style.display = "none";
+  document.getElementById("E2").style.display = "none";
+  document.getElementById("E3").style.display = "none";
+  document.getElementById("E4").style.display = "none";
+  document.getElementById("E5").style.display = "none";
+  document.getElementById("E6").style.display = "none";
+  document.getElementById("E7").style.display = "none";
+
+  document.getElementById("C1").style.display = "none";
+  document.getElementById("C2").style.display = "none";
+  document.getElementById("C3").style.display = "none";
+  document.getElementById("C4").style.display = "none";
+  document.getElementById("C5").style.display = "none";
+  document.getElementById("C6").style.display = "none";
+  document.getElementById("C7").style.display = "none";
+  return;
+}
